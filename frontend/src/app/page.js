8c711d436118199ad9b6ff42c92d2634381ee595
Swanand-Wagh <swanu@uw.edu>
2024-07-12 12:47:13 -0700

    skeleton folder structure

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const homepage = () => {
-  return <div>hello world!</div>;
+export const HomePage = () => {
+  return <>hello world!</>;
 };
 
-export default homepage;
+export default HomePage;
