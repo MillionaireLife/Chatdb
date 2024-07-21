6c5fe070692c4f01f5fc6d5632aaa931b7f85d66
Swanand-Wagh <swanu@uw.edu>
2024-07-20 19:55:32 -0700

    class -> className

--- frontend/src/app/_components/hero/hero.js
+++ frontend/src/app/_components/hero/hero.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
+
 import { TextInput } from '../elements/textinput';
 import styles from './hero.module.css';
-import Image from 'next/image';
 
 export const HeroBody = () => {
   return (
     <>
-      <div class={styles.heroContainer}>
+      <div className={styles.heroContainer}>
         <Image src="/logos/dblogo.png" alt="Logo" position="center" width={70} height={60} />
-        <h1 class={styles.heroTitle}>Welcome to ChatDB</h1>
-        <h2 class={styles.heroSubtitle}>Seamless Conversations with Your Database - No Queries, Just Answers</h2>
+
+        <h1 className={styles.heroTitle}>Welcome to ChatDB</h1>
+        <h2 className={styles.heroSubtitle}>Seamless Conversations with Your Database - No Queries, Just Answers</h2>
       </div>
+
       <TextInput />
     </>
   );
