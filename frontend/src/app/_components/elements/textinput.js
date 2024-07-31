05dc477e631b5fe5f8b31ad5e809818994fcede8
Swanand-Wagh <swanu@uw.edu>
2024-07-30 17:09:47 -0700

    user can hit enter to see query on screen

--- frontend/src/app/_components/elements/textinput.js
+++ frontend/src/app/_components/elements/textinput.js
@@ -1,67 +1,51 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+
+import React, { useState } from 'react';
 import { ActionIcon, Input } from '@mantine/core';
 import { BsArrowUpCircleFill } from 'react-icons/bs';
 
-export const TextInput = ({ userinput, setuserinput, setDisplaytext }) => {
-  console.log(userinput);
-  function submit(e){
-    if (e.key === 'Enter') {
-      console.log('Enter key pressed');
-      // setuserinput(e.currentTarget.value);
-      setDisplaytext(e.currentTarget.value);
-      console.log(userinput);
+export const TextInput = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      onSubmit(query);
+      setQuery(''); 
     }
-  }
-  useEffect(() => {
-    const submit = (event) => {
-      if (event.key === 'Enter') {
-        setDisplaytext(event.currentTarget.value);
-      }
-    };
+  };
 
-    // Add event listener for keydown
-    window.addEventListener('keydown', submit);
+  const handleClick = () => {
+    onSubmit(query);
+    setQuery(''); 
+  };
 
-    // Clean up event listener on component unmount
-    return () => {
-      window.removeEventListener('keydown', submit);
-    };
-  }, []);
-  // useEffect(() => {
-  //   document.addEventListener('keydown', submit);
-  //   return () => {
-  //     document.removeEventListener('keydown', submit);
-  //   };
-  // }, []);
   return (
-    <>
-      <Input
-        size="md"
-        bottom={0}
-        w={'100%'}
-        radius="md"
-        pos={'fixed'}
-        value={userinput}
-        bg={'#f0f0f0'}
-        p={'0.85rem 7.5rem'}
-        rightSectionWidth={300}
-        rightSectionPointerEvents="all"
-        placeholder="Enter your query here..."
-        onChange={(e) => setuserinput(e.currentTarget.value)}
-        rightSection={
-          <ActionIcon
-            size="md"
-            radius="xl"
-            color="dark"
-            variant="default"
-            aria-label="Send"
-            onClick={() => setDisplaytext(userinput)}
-          >
-            <BsArrowUpCircleFill style={{ width: '30px', height: '30px' }} />
-          </ActionIcon>
-        }
-      />
-    </>
+    <Input
+      size="md"
+      bottom={0}
+      w={'100%'}
+      radius="md"
+      pos={'fixed'}
+      value={query}
+      bg={'#f0f0f0'}
+      p={'0.85rem 7.5rem'}
+      rightSectionWidth={300}
+      rightSectionPointerEvents="all"
+      placeholder="Enter your query here..."
+      onChange={(e) => setQuery(e.currentTarget.value)}
+      onKeyDown={handleKeyPress}
+      rightSection={
+        <ActionIcon
+          size="md"
+          radius="xl"
+          color="dark"
+          variant="default"
+          aria-label="Send"
+          onClick={handleClick}
+        >
+          <BsArrowUpCircleFill style={{ width: '30px', height: '30px' }} />
+        </ActionIcon>
+      }
+    />
   );
 };
--- frontend/src/app/_components/hero/hero.js
+++ frontend/src/app/_components/hero/hero.js
@@ -1,24 +1,33 @@
 'use client';
+
 import React, { useState } from 'react';
-import { TextInput } from '../elements/textinput';
 import styles from './hero.module.css';
 import Image from 'next/image';
 
+import { TextInput } from '../elements/textinput';
+
 export const HeroBody = () => {
-  const [query, setQuery] = useState(''); // let query=""
-  const [displaytext, setDisplaytext] = useState('');
-console.log(query);
+  const [displayText, setDisplayText] = useState(null);
+
+  const handleTextSubmit = (text) => {
+    setDisplayText(text);
+  };
+
   return (
     <>
-      {/* <div className={styles.heroContainer}>
-        <Image src="/logos/dblogo.png" alt="Logo" position="center" width={70} height={60} />
-        <h1 className={styles.heroTitle}>Welcome to ChatDB</h1>
-        <h2 className={styles.heroSubtitle}>Seamless Conversations with Your Database - No Queries, Just Answers</h2>
-      </div> */}
-      <p>{displaytext}</p>
-      <TextInput userinput={query} setuserinput={setQuery} setDisplaytext={setDisplaytext} />{' '}
-      {/*userinput = value stored in the query variable */}
-      {/* <input onChange={(e) => setQuery(e.target.value)} value = {query}></input> */}
+      <div className={styles.hero_container}>
+        {/* <div className={styles.hero_default_section}>
+          <Image src="/logos/dblogo.png" alt="Logo" position="center" width={70} height={60} />
+          <h1 className={styles.heroTitle}>Welcome to ChatDB</h1>
+          <h2 className={styles.heroSubtitle}>Seamless Conversations with Your Database - No Queries, Just Answers</h2>
+        </div> */}
+
+        <div className={styles.display_section}>
+          <p>{displayText}</p>
+        </div>
+      </div>
+
+      <TextInput onSubmit={handleTextSubmit} />
     </>
   );
 };
